814ad90c223ae52548598a3d83a634b5d4d1b765
drumnation <davidmieloch@gmail.com>
2017-06-08 23:58:25 -0400

    getting routes setup before bed

--- auto-metal-api/db/adapters/spotify_search_for_an_item.rb
+++ auto-metal-api/db/adapters/spotify_search_for_an_item.rb
@@ -3,8 +3,7 @@ require_relative 'request'
 
 def get_spotify_track_queries_from_csv
     artist_song_migration_array = File.open('./db/junk_and_band_song_names.csv').map do | line |
-        lines = line.gsub('"', '').gsub("\r\n", "").split(',')
-        { artist: lines[0], song: lines[1] }
+        lines = line.gsub('"', '').gsub("\r\n", "").split(',') { artist: lines[0], song: lines[1] }
     end.uniq!
     artist_song_migration_array.collect { | line | "#{line[:artist]} #{line[:song]}" }
 end
