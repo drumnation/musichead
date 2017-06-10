e4872001b1f52913ddb19bbf5ee73699fd044734
drumnation <davidmieloch@gmail.com>
2017-06-10 10:04:07 -0400

    changed auth table migration to a users table and began the process of setting up omniauth

--- auto-metal-api/db/migrate/20170609230235_add_oauth_table.rb
+++
@@ -1,8 +0,0 @@
-class AddOauthTable < ActiveRecord::Migration[5.0]
-  def change
-    create_table :oauth do |t|
-      t.string :spotify_token
-      t.timestamps
-    end
-  end
-end
---
+++ auto-metal-api/db/migrate/20170609230235_add_users_table.rb
@@ -0,0 +1,10 @@
+class AddUsersTable < ActiveRecord::Migration[5.0]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :provider
+      t.string :uid
+      t.timestamps
+    end
+  end
+end
