a3c9dceadc0dae6df23fd47f2f452e6c838ca2f6
drumnation <davidmieloch@gmail.com>
2017-06-19 04:27:27 -0400

    added route change so root points to the user profile.

--- auto-metal-jsclient/src/components/home/homeLoggedIn.jsx
+++ auto-metal-jsclient/src/components/home/homeLoggedIn.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import SearchForArtist from './search/searchForArtist'
 import SearchForTrack from './search/searchForTrack'
 import SearchForAlbum from './search/searchForAlbum'
+import UserShow from '../../containers/userShow'
 import HomeFeed from './homeFeed'
 import { Switch, Route, withRouter } from 'react-router-dom'
 
@@ -23,7 +24,7 @@ class HomeLoggedIn extends Component {
         return(
             <Switch>
                 {/*<Route exact path='/' component={HomeFeed}/>*/}
-                <Route exact path='/' component={SearchForTrack}/>
+                <Route exact path='/' component={UserShow}/>
                 <Route path='/artist' component={SearchForArtist}/>
                 <Route path='/album' component={SearchForAlbum}/>
                 <Route path='/track' component={SearchForTrack}/>
